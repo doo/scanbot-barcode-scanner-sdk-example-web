74b636d2352193e40d4de079e78166c261a7f6c3
Angel Tasevski <tupacan@gmail.com>
2023-11-14 10:12:50 +0100

    minor fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="py-2 text-center border-t border-gray-200">
       <div className="container">
-        <p className="text-sm">
-          ©Scanbot SDK GmbH 2011-2023, located in Germany and the US
+        <p className="text-sm text-gray-800">
+          ©Scanbot SDK GmbH 2011-{new Date().getFullYear()}
         </p>
       </div>
     </footer>
