9b924384c7b24cade71949d97240589b4f754a04
as-bowley <alexstubowley@gmail.com>
2023-12-06 11:18:09 +0100

    update findAndPickARScan config

--- barcode-usecases/src/utils/findAndPickARScan.ts
+++ barcode-usecases/src/utils/findAndPickARScan.ts
@@ -2,25 +2,24 @@ import { Barcode } from "scanbot-web-sdk/@types/model/barcode/barcode";
 import { BarcodeResult } from "scanbot-web-sdk/@types/model/barcode/barcode-result";
 import { IBarcodePolygonHandle, IBarcodePolygonLabelHandle } from "scanbot-web-sdk/@types/model/configuration/selection-overlay-configuration";
 
-const findAndPickARScan = {
+const findAndPickARScan = (updateResults: (result: BarcodeResult) => void) => ({
   containerId: "scanner",
-  onBarcodesDetected: (result: BarcodeResult) => {
-    console.log(result);
-  },
+	returnBarcodeImage: true,
   onError: (error: Error) => {
     console.log(error);
   },
-	style: {
-		hint: "Please scan a QR code to see FindAndPick in action!",
-	},
+  showFinder: false,
   overlay: {
     visible: true,
+		returnBarcodeImage: true,
     onBarcodeFound: (
       code: Barcode,
       polygon: IBarcodePolygonHandle,
       label: IBarcodePolygonLabelHandle
     ) => {
       if (code.format === "QR_CODE") {
+				console.log(code);
+				updateResults({ barcodes: [code] });
         polygon.style({
           fill: "rgba(0, 255, 0, 0.3)",
           stroke: "rgba(0, 255, 0, 1)",
@@ -29,10 +28,18 @@ const findAndPickARScan = {
           backgroundColor: "rgba(0, 255, 0, 0.9)",
           textColor: "white",
         });
-      }
+      } else {
+				polygon.style({
+					fill: "rgba(255, 255, 255, 0.3)",
+					stroke: "rgba(255, 255, 255, 1)",
+				});
+				label.style({
+					backgroundColor: "rgba(255, 255, 255, 0.9)",
+					textColor: "black",
+				});
+			}
     },
   },
-};
-
+});
 
 export default findAndPickARScan;
